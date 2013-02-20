983dd2ea78acb730842d768758e9b12b03180675
Jason Long <jlong@messiah.edu>
2013-02-20 16:15:58 -0500

    home screen: show last submission result next to each problem

--- show_problems_list.inc.php
+++ show_problems_list.inc.php
@@ -1,13 +1,23 @@
 <?php
 
-$sql = "SELECT p.problem_number,problem_name,spec_file,spec_name,
-	thetime,incorrect_submissions,opened,difficulty
+$sql = "
+	SELECT problem_number,problem_name,spec_file,spec_name,
+		thetime,incorrect_submissions,opened,difficulty,
+		s.submitted,s.status
+	FROM (
+	SELECT p.problem_number,problem_name,spec_file,spec_name,
+	thetime,incorrect_submissions,opened,difficulty,
+	(SELECT MAX(id) FROM submission WHERE team=r.team_number
+		AND problem=p.problem_number) AS last_submission
 	FROM problem p
 	LEFT JOIN results r
 	ON r.problem_number=p.problem_number
 	AND team_number=".db_quote($team_info['team_number'])."
 	WHERE contest=" . db_quote($contest_id) . "
 	AND ".check_phase_option_bool('pp_read_problem')."
+	) t1
+	LEFT JOIN submission s
+		ON s.id=t1.last_submission
 	ORDER BY problem_number
 		";
 $result = mysql_query($sql)
@@ -23,11 +33,19 @@ while ($row) {
 	$open_url = "open_problem.php?problem=" . urlencode($row['problem_number']);
 	$status_h = "Unopened";
 	if ($row['thetime']) {
-		$status_h = "Solved";
+		$status_h = "Solved!";
 	} else if ($row['incorrect_submissions']) {
-		$status_h = "Attempted";
+		$status_h = "Attempted " . $row['incorrect_submissions'] . " times";
+		if ($row['submitted']) {
+			$status_h .= "; last submitted ".format_sqldatetime($row['submitted']) . " (".htmlspecialchars($row['status']).")";
+		}
+	} else if ($row['submitted']) {
+		$status_h = "Submitted ".format_sqldatetime($row['submitted']);
+		if ($row['status']) {
+			$status_h .= " (" . htmlspecialchars($row['status']).")";
+		}
 	} else if ($row['opened']) {
-		$status_h = "Opened ".format_sqldatetime($row['opened']);
+		$status_h = "Opened ".format_sqldatetime($row['opened']). " $row[submission_count]";
 	}
 ?>
 <tr><td><a href="<?php echo htmlspecialchars($open_url)?>">
