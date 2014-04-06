7620c325e42481ded23e2ae9dea7ba6fb7119235
Jason Long <jason@long.name>
2014-04-06 11:36:13 -0400

    begin work of app-engine conversion

---
+++ testserver.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm -rf war
+mkdir war
+(
+cd war
+jar -xvf ../falcon-contest.war
+)
+cp -v appengine-web.xml war/WEB-INF/
+
+cmd.exe /c 'C:\cygwin\home\jason\appengine-java-sdk-1.8.9\bin\dev_appserver.cmd war'
