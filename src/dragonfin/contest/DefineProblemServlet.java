3ee0aff91f80f0883af0789b079284a219ed9e02
Jason Long <jason@long.name>
2014-04-07 12:03:16 -0400

    fix for handling form/multi-part data

--- src/dragonfin/contest/DefineProblemServlet.java
+++ src/dragonfin/contest/DefineProblemServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import dragonfin.contest.model.*;
 import com.google.appengine.api.datastore.*;
+import org.apache.commons.fileupload.*;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import java.nio.charset.Charset;
 
 public class DefineProblemServlet extends CoreServlet
 {
@@ -17,13 +20,56 @@ public class DefineProblemServlet extends CoreServlet
 		renderTemplate(req, resp, TEMPLATE);
 	}
 
+	static final Charset UTF8 = Charset.forName("UTF-8");
+
+	Map<String,String> processMultipartForm(HttpServletRequest req)
+		throws ServletException, IOException
+	{
+		try {
+
+		Map<String,String> formFields = new HashMap<String,String>();
+
+		ServletFileUpload upload = new ServletFileUpload();
+		FileItemIterator it = upload.getItemIterator(req);
+		while (it.hasNext()) {
+			FileItemStream item = it.next();
+			InputStream stream = item.openStream();
+
+			if (item.isFormField()) {
+				String name = item.getFieldName();
+				ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+				int nread;
+				byte[] buf = new byte[8192];
+				while ( (nread = stream.read(buf)) != -1) {
+					bytes.write(buf, 0, nread);
+				}
+
+				String value = new String(bytes.toByteArray(), UTF8);
+				formFields.put(name, value);
+			}
+			else {
+				//TODO
+				throw new ServletException("Unable to handle file upload at this time.");
+			}
+		}
+
+		req.setAttribute("POST", formFields);
+		return formFields;
+
+		} catch (FileUploadException e) {
+			throw new ServletException(e);
+		}
+	}
+
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 		throws IOException, ServletException
 	{
-		if (req.getParameter("action:cancel") != null) {
+		Map<String,String> POST = processMultipartForm(req);
+
+		if (POST.containsKey("action:cancel")) {
 			doCancel(req, resp);
 		}
-		else if (req.getParameter("action:create_problem") != null) {
+		else if (POST.containsKey("action:create_problem")) {
 			doCreateProblem(req, resp);
 		}
 		else {
@@ -91,7 +137,7 @@ public class DefineProblemServlet extends CoreServlet
 	{
 		String contestId = req.getParameter("contest");
 		HttpSession s = req.getSession(false);
-		if (s != null) {
+		if (s == null) {
 			throw new ServletException("Not logged in");
 		}
 
