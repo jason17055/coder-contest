2d5cb8b9ad7ecf6215b601c171a845e3105db49d
Jason Long <jason@long.name>
2014-04-07 22:05:15 -0400

    more work on file-upload capability

--- src/dragonfin/contest/DefineProblemServlet.java
+++ src/dragonfin/contest/DefineProblemServlet.java
@@ -5,6 +5,7 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import dragonfin.contest.model.*;
+import dragonfin.contest.model.File;
 import com.google.appengine.api.datastore.*;
 import org.apache.commons.fileupload.*;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
@@ -75,47 +76,113 @@ public class DefineProblemServlet extends CoreServlet
 		return sb.toString();
 	}
 
-	String handleFileUpload(FileItemStream item)
+	static class UploadHelper
+	{
+		DatastoreService ds;
+		InputStream stream;
+		byte [] buf = new byte[16*1024];
+
+		static MessageDigest createDigestObject()
+		{
+			try {
+				return MessageDigest.getInstance("SHA-1");
+			}
+			catch (NoSuchAlgorithmException e) {
+				throw new Error("Unexpected "+e.getMessage(), e);
+			}
+		}
+
+		MessageDigest md = createDigestObject();
+
+		FilePart nextChunk()
+			throws IOException
+		{
+			int nread = stream.read(buf);
+			if (nread == -1) {
+				return null;
+			}
+
+			FilePart p = new FilePart();
+			p.data = Arrays.copyOfRange(buf, 0, nread);
+			makeDigest(p);
+			return p;
+		}
+
+		void makeDigest(FilePart p)
+		{
+			md.update(p.data);
+			byte [] digestBytes = md.digest();
+			p.digestHex = byteArray2Hex(digestBytes);
+		}
+
+		Key[] processStream(InputStream stream)
+			throws IOException
+		{
+			this.stream = stream;
+
+			ArrayList<Key> chunks = new ArrayList<Key>();
+
+			FilePart p;
+			while ( (p = nextChunk()) != null )
+			{
+				Key chunkKey = KeyFactory.createKey("FileChunk", p.digestHex);
+				Entity ent = new Entity(chunkKey);
+				ent.setProperty("data", p.data);
+				ds.put(ent);
+
+				chunks.add(chunkKey);
+			}
+
+			return chunks.toArray(new Key[0]);
+		}
+	}
+
+	static class FilePart
+	{
+		byte [] data;
+		String digestHex;
+	}
+
+	File handleFileUpload(FileItemStream item)
 		throws ServletException, IOException
 	{
 		String fileName = item.getName();
 		String contentType = item.getContentType();
 
+		UploadHelper helper = new UploadHelper();
+		helper.ds = DatastoreServiceFactory.getDatastoreService();
+
 		InputStream stream = item.openStream();
+		Key [] chunks = helper.processStream(stream);
 
-		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-		MessageDigest md;
-		try {
-			md = MessageDigest.getInstance("SHA-1");
-		}
-		catch (NoSuchAlgorithmException e) {
-			throw new Error("Unexpected "+e.getMessage(), e);
-		}
-
-		byte [] buf = new byte[16*1024];
-		int nread;
-		String sha1_hex = null;
-		while ( (nread = stream.read(buf)) != -1 ) {
-
-			// compute SHA-1 hash of bytes
-			md.update(buf, 0, nread);
-			byte[] hash = md.digest();
-			sha1_hex = byteArray2Hex(hash);
-
-			Key blobKey = KeyFactory.createKey("File", sha1_hex);
-			Entity ent = new Entity(blobKey);
-
-			byte [] bytes = Arrays.copyOfRange(buf, 0, nread);
-			ent.setProperty("data", bytes);
-			ent.setProperty("uploaded", new Date());
-			ent.setProperty("given_name", fileName);
-			ent.setProperty("content_type", contentType);
-			ent.setProperty("size", new Long(nread));
-
-			ds.put(ent);
+		helper.md.reset();
+		helper.md.update(fileName.getBytes(UTF8));
+		helper.md.update((byte)0);
+		helper.md.update(contentType.getBytes(UTF8));
+		helper.md.update((byte)0);
+		for (Key chunkKey : chunks) {
+			String name = chunkKey.getName();
+			helper.md.update(name.getBytes(UTF8));
 		}
 
-		return sha1_hex;
+		byte[] digestBytes = helper.md.digest();
+		String digestHex = byteArray2Hex(digestBytes);
+
+		Key fileKey = KeyFactory.createKey("File", digestHex);
+		Entity ent = new Entity(fileKey);
+
+		ent.setProperty("uploaded", new Date());
+		ent.setProperty("given_name", fileName);
+		ent.setProperty("content_type", contentType);
+
+		ent.setProperty("chunks", chunks);
+		
+		helper.ds.put(ent);
+
+		File f = new File();
+		f.id = digestHex;
+		f.name = fileName;
+		return f;
 	}
 
 	Map<String,String> processMultipartForm(HttpServletRequest req)
@@ -138,7 +205,8 @@ public class DefineProblemServlet extends CoreServlet
 			else {
 				String name = item.getFieldName();
 				String fileName = item.getName();
-				String blobId = handleFileUpload(item);
+				File f = handleFileUpload(item);
+				formFields.put(name, f.name);
 			}
 		}
 
