15b46598a35c68524ece7d66265eda1325360e49
Jason Long <jason@long.name>
2014-04-08 08:44:32 -0400

    admin: implement an /_admin/chunks utility interface

---
+++ src/dragonfin/contest/AdminFileChunksServlet.java
@@ -0,0 +1,71 @@
+package dragonfin.contest;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.google.appengine.api.datastore.*;
+
+public class AdminFileChunksServlet extends CoreServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body>");
+		out.println("<h2>File Chunks</h2>");
+		out.println("<form method='post' action='chunks'>");
+		out.println("<table border='1'>");
+
+		Query q = new Query("FileChunk")
+			;
+		PreparedQuery pq = ds.prepare(q);
+
+		for (Entity ent : pq.asIterable()) {
+			String name = ent.getKey().getName();
+			Date touched = (Date) ent.getProperty("last_touched");
+
+			@SuppressWarnings("unchecked")
+			List<Key> partsList = (List<Key>) ent.getProperty("parts");
+
+			Blob bytes = (Blob) ent.getProperty("data");
+
+			String content =
+			partsList != null ? String.format("%d parts", partsList.size()) :
+			bytes != null ? String.format("%d bytes", bytes.getBytes().length) : "";
+
+			out.println("<tr>");
+			out.println("<td>"+name+"</td>");
+			out.println("<td>"+touched+"</td>");
+			out.println("<td>"+content+"</td>");
+			out.println("</tr>");
+		}
+		out.println("</table>");
+		out.println("<div>");
+		out.println("<input type='text' name='chunk' size='40'>");
+		out.println("<button type='submit' name='action:delete_chunk'>Delete Chunk</button>");
+		out.println("</div>");
+		out.println("</form>");
+		out.println("</body></html>");
+		out.close();
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		if (req.getParameter("action:delete_chunk") != null) {
+
+			DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+			String chunkName = req.getParameter("chunk");
+			Key chunkKey = KeyFactory.createKey("FileChunk", chunkName);
+			ds.delete(chunkKey);
+
+			resp.sendRedirect("chunks");
+		}
+		else {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+		}
+	}
+}
