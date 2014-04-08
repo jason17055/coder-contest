e6d4f897bd6eb940536997419cda9a02ca9db2e4
Jason Long <jason@long.name>
2014-04-08 08:19:03 -0400

    upload-form: handle large files

--- testserver.sh
+++ testserver.sh
@@ -7,5 +7,6 @@ jar -xvf ../falcon-contest.war
 )
 cp -v appengine-web.xml war/WEB-INF/
 
-appenginedir=$(cygpath -w -a ~/appengine-java-sdk-1.8.9)
+test -z "$APPENGINE_SDK" && APPENGINE_SDK=~/appengine-java-sdk-1.8.9
+appenginedir=$(cygpath -w -a "$APPENGINE_SDK")
 cmd.exe /c "start $appenginedir"'\bin\dev_appserver.cmd war'
