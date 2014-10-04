a08eca7958ce18762a791ce5fa9d66835ae600e6
Jason Long <jason@long.name>
2014-10-04 19:38:22 -0400

    edit_submission: implement Cancel button

--- src/dragonfin/contest/EditSubmissionServlet.java
+++ src/dragonfin/contest/EditSubmissionServlet.java
@@ -10,12 +10,17 @@ import com.google.appengine.api.datastore.*;
 
 public class EditSubmissionServlet extends CoreServlet
 {
+	String getTemplate()
+	{
+		return "edit_submission.tt";
+	}
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 		throws IOException, ServletException
 	{
 		if (requireDirector(req, resp)) { return; }
 
-		renderTemplate(req, resp, "edit_submission.tt");
+		renderTemplate(req, resp, getTemplate());
 	}
 
 	void moreVars(TemplateVariables tv, SimpleBindings ctx)
@@ -24,5 +29,30 @@ public class EditSubmissionServlet extends CoreServlet
 		String contestId = tv.req.getParameter("contest");
 		String id = tv.req.getParameter("id");
 		ctx.put("submission", tv.fetchSubmission(contestId, id));
+	}
+
+	FileUploadFormHelper uploadForm = new FileUploadFormHelper();
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		Map<String,String> POST = uploadForm.processMultipartForm(req);
+
+		if (POST.containsKey("action:cancel")) {
+			doCancel(req, resp);
+		}
+		else {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		}
+	}
+
+	void doCancel(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		String u = req.getParameter("next");
+		if (u == null) {
+			u = makeContestUrl(req.getParameter("contest"), "submissions", null);
+		}
+		resp.sendRedirect(u);
 	}
 }
