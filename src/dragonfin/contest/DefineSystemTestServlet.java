3176137b7a6437f81bbf76314a953be898db56b8
Jason Long <jason@long.name>
2014-10-04 22:02:07 -0400

    new page: implement define_system_test

---
+++ src/dragonfin/contest/DefineSystemTestServlet.java
@@ -0,0 +1,183 @@
+package dragonfin.contest;
+
+import java.io.*;
+import java.util.*;
+import javax.script.SimpleBindings;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import dragonfin.contest.model.*;
+import dragonfin.contest.model.File;
+import com.google.appengine.api.datastore.*;
+
+public class DefineSystemTestServlet extends CoreServlet
+{
+	public String getTemplate()
+	{
+		return "define_system_test.tt";
+	}
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		if (requireDirector(req, resp)) { return; }
+		renderTemplate(req, resp, getTemplate());
+	}
+
+	void moreVars(TemplateVariables tv, SimpleBindings ctx)
+		throws Exception
+	{
+		String contestId = tv.req.getParameter("contest");
+		String problemId = tv.req.getParameter("problem");
+		String id = tv.req.getParameter("id");
+
+		if (id != null) {
+
+			//TODO
+
+			Map<String,Object> form = new HashMap<String,Object>();
+			ctx.put("f", form);
+		}
+		else {
+
+			TemplateVariables.Problem p = tv.fetchProblem(contestId, problemId);
+			ctx.put("problem", p);
+
+			Map<String,Object> form = new HashMap<String,Object>();
+			form.put("problem", p);
+			ctx.put("f", form);
+		}
+	}
+
+	FileUploadFormHelper uploadForm = new FileUploadFormHelper();
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		Map<String,String> POST = uploadForm.processMultipartForm(req);
+
+		if (POST.containsKey("action:cancel")) {
+			doCancel(req, resp);
+		}
+		else if (POST.containsKey("action:create_system_test")) {
+			doCreateSystemTest(req, resp);
+		}
+		else {
+			doUpdateSystemTest(req, resp);
+		}
+	}
+
+	void doCancel(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		String u = req.getParameter("next");
+		if (u == null) {
+			u = makeContestUrl(req.getParameter("contest"), "problems", null);
+		}
+		resp.sendRedirect(u);
+	}
+
+	void doCreateSystemTest(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		String contestId = req.getParameter("contest");
+		String problemId = req.getParameter("problem");
+		if (requireDirector(req, resp)) { return; }
+
+		@SuppressWarnings("unchecked")
+		Map<String,String> POST = (Map<String,String>) req.getAttribute("POST");
+
+		// TODO- check parameters
+
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		Transaction txn = ds.beginTransaction();
+
+		try {
+
+			Key contestKey = KeyFactory.createKey("Contest", contestId);
+			Key problemKey = KeyFactory.createKey(contestKey, "Problem", Long.parseLong(problemId));
+			Entity problemEnt = ds.get(problemKey);
+
+			long testId = problemEnt.hasProperty("last_system_test_id") ?
+				((Long)problemEnt.getProperty("last_system_test_id")).longValue() : 0;
+			testId++;
+			problemEnt.setProperty("last_system_test_id", new Long(testId));
+			ds.put(problemEnt);
+
+			Key systestKey = KeyFactory.createKey(problemKey,
+				"SystemTest", testId);
+			Entity ent = new Entity(systestKey);
+			ent.setProperty("created", new Date());
+			updateFromForm(ent, POST);
+			ds.put(ent);
+
+			txn.commit();
+		}
+		catch (EntityNotFoundException e) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		finally {
+			if (txn.isActive()) {
+				txn.rollback();
+			}
+		}
+
+		doCancel(req, resp);
+	}
+
+	void updateFromForm(Entity ent, Map<String,String> POST)
+	{
+		boolean isSample = POST.containsKey("sample");
+		boolean isAutoJudge = POST.containsKey("auto_judge");
+
+		ent.setProperty("sample", new Boolean(isSample));
+		ent.setProperty("auto_judge", new Boolean(isAutoJudge));
+
+		updateFromForm_file(ent, POST, "input");
+		updateFromForm_file(ent, POST, "expected");
+	}
+
+	void doUpdateSystemTest(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		String contestId = req.getParameter("contest");
+		String problemId = req.getParameter("problem");
+		String testNumber = req.getParameter("number");
+		if (requireDirector(req, resp)) { return; }
+
+		@SuppressWarnings("unchecked")
+		Map<String,String> POST = (Map<String,String>) req.getAttribute("POST");
+
+		// TODO- check parameters
+
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		Transaction txn = ds.beginTransaction();
+
+		try {
+
+			Key contestKey = KeyFactory.createKey("Contest", contestId);
+			Key prbKey = KeyFactory.createKey(contestKey,
+				"Problem", Long.parseLong(problemId));
+			Key testKey = KeyFactory.createKey(prbKey,
+				"SystemTest", Long.parseLong(testNumber));
+
+			Entity ent1 = ds.get(testKey);
+			updateFromForm(ent1, POST);
+			ds.put(ent1);
+
+			txn.commit();
+		}
+		catch (EntityNotFoundException e) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST,
+				"Invalid System Test.");
+			return;
+		}
+		finally {
+			if (txn.isActive()) {
+				txn.rollback();
+			}
+		}
+
+		doCancel(req, resp);
+	}
+}
