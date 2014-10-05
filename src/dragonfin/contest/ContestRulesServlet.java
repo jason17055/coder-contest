cf14fdf4a6cae46ae429fc59b576977f6898b561
Jason Long <jason@long.name>
2014-10-04 21:03:16 -0400

    rules page: implement update_rules action

--- src/dragonfin/contest/ContestRulesServlet.java
+++ src/dragonfin/contest/ContestRulesServlet.java
@@ -36,7 +36,7 @@ public class ContestRulesServlet extends CoreServlet
 			doCancel(req, resp);
 		}
 		else {
-			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			doUpdateRules(req, resp);
 		}
 	}
 
@@ -48,5 +48,39 @@ public class ContestRulesServlet extends CoreServlet
 			u = makeContestUrl(req.getParameter("contest"), "", null);
 		}
 		resp.sendRedirect(u);
+	}
+
+	void doUpdateRules(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		if (requireDirector(req, resp)) { return; }
+
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		Transaction txn = ds.beginTransaction();
+
+		try {
+
+			String contestId = req.getParameter("contest");
+			Key contestKey = KeyFactory.createKey("Contest", contestId);
+
+			Entity ent = ds.get(contestKey);
+			ent.setProperty("title", req.getParameter("title"));
+			ent.setProperty("subtitle", req.getParameter("subtitle"));
+			ent.setProperty("logo", req.getParameter("logo"));
+
+			//TODO- the remaining parameters on this form.
+
+			ds.put(ent);
+
+			txn.commit();
+		}
+		catch (EntityNotFoundException e) {
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+		}
+		finally {
+			if (txn.isActive()) {
+				txn.rollback();
+			}
+		}
 	}
 }
