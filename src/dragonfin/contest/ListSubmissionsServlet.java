087ed2b48a07ead7e239b0da232f2fb846316d2e
Jason Long <jason@long.name>
2014-11-07 16:23:40 -0500

    list_submissions: allow judges to see this page

--- src/dragonfin/contest/ListSubmissionsServlet.java
+++ src/dragonfin/contest/ListSubmissionsServlet.java
@@ -8,10 +8,31 @@ import com.google.appengine.api.datastore.*;
 
 public class ListSubmissionsServlet extends CoreServlet
 {
+	boolean requireJudge(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		if (requireContest(req, resp)) { return true; }
+
+		TemplateVariables tv = makeTemplateVariables(req);
+		try {
+			TemplateVariables.User u = tv.fetchUser(getLoggedInUserKey(req));
+			if (u.is_director || u.is_judge) {
+				return false;
+			}
+		}
+		catch (EntityNotFoundException e) {
+		}
+
+		// not a judge or director
+		resp.sendError(HttpServletResponse.SC_FORBIDDEN,
+			"This page requires director or judge access.");
+		return true;
+	}
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 		throws IOException, ServletException
 	{
-		if (requireDirector(req, resp)) { return; }
+		if (requireJudge(req, resp)) { return; }
 
 		renderTemplate(req, resp, "list_submissions.tt");
 	}
