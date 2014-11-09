e4022b0a65dfeb9084b1bad3e1d0c34c5a7761a2
Jason Long <jason@long.name>
2014-11-09 11:53:00 -0500

    issue_credentials: implement resetting of passwords

--- src/dragonfin/contest/IssueCredentialsServlet.java
+++ src/dragonfin/contest/IssueCredentialsServlet.java
@@ -6,14 +6,19 @@ import dragonfin.templates.Function;
 
 import java.io.*;
 import java.util.*;
+import java.util.logging.Logger;
 import javax.script.Bindings;
 import javax.script.SimpleBindings;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import com.google.appengine.api.datastore.*;
 
+import static dragonfin.contest.TemplateVariables.makeUserKey;
+
 public class IssueCredentialsServlet extends CoreServlet
 {
+	private static final Logger log = Logger.getLogger(IssueCredentialsServlet.class.getName());
+
 	String getTemplate()
 	{
 		return "issue_credentials.tt";
@@ -43,7 +48,7 @@ public class IssueCredentialsServlet extends CoreServlet
 			doCancel(req, resp);
 		}
 		else {
-			resp.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED);
+			doSetPasswords(req, resp);
 		}
 	}
 
@@ -55,6 +60,54 @@ public class IssueCredentialsServlet extends CoreServlet
 			u = makeContestUrl(req.getParameter("contest"), "users", null);
 		}
 		resp.sendRedirect(u);
+	}
+
+	void doSetPasswords(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException, ServletException
+	{
+		String contestId = req.getParameter("contest");
+		if (contestId == null) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+
+		for (Enumeration<String> e = req.getParameterNames(); e.hasMoreElements(); ) {
+			String pname = e.nextElement();
+			if (pname.startsWith("reset:")) {
+				String username = pname.substring(6);
+				String pass = req.getParameter("password:"+username);
+				doSetOnePassword(ds, contestId, username, pass);
+			}
+		}
+
+	}
+
+	void doSetOnePassword(DatastoreService ds, String contestId, String username, String pass)
+		throws ServletException
+	{
+		log.info("resetting password for "+username);
+
+		Key userKey = makeUserKey(contestId, username);
+
+		Transaction txn = ds.beginTransaction();
+
+		try {
+
+			Entity ent = ds.get(userKey);
+			ent.setProperty("password", pass);
+			ds.put(ent);
+			txn.commit();
+		}
+		catch (EntityNotFoundException e) {
+			throw new ServletException("Entity unexpectedly not found in datastore.");
+		}
+		finally {
+			if (txn.isActive()) {
+				txn.rollback();
+			}
+		}
 	}
 
 	static final String PASSWORD_CHARS = "2346789bcdfghjkmnpqrtvwxyzBCDFGHJKLMNPQRTVWXYZ";
