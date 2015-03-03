1949f5a0e8fb3242e4a10bd49d29e9417d03d527
Jason Long <jason@long.name>
2015-03-02 18:47:13 -0800

    test result page: make link to repeat test with same source/input

--- src/dragonfin/contest/TestResultServlet.java
+++ src/dragonfin/contest/TestResultServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import javax.script.SimpleBindings;
 import com.google.appengine.api.datastore.*;
 
+import static dragonfin.contest.common.CommonFunctions.escapeUrl;
+
 public class TestResultServlet extends ProblemCoreServlet
 {
 	@Override
@@ -18,9 +20,21 @@ public class TestResultServlet extends ProblemCoreServlet
 	{
 		super.moreVars(tv, ctx);
 
+		String contestId = tv.req.getParameter("contest");
+		String problemId = tv.req.getParameter("problem");
 		String jobId = tv.req.getParameter("id");
 
 		TemplateVariables.TestJob j = tv.fetchTestJob(jobId);
 		ctx.put("test_job", j);
+
+		String problemUrl = makeContestUrl(contestId, "problem."+problemId+"/");
+		String anotherTestUrl = problemUrl + "test?source=" + escapeUrl(j.sourceFileKey.getName());
+		if (j.inputFileKey != null) {
+			anotherTestUrl += "&input=" + escapeUrl(j.inputFileKey.getName());
+		}
+		if (tv.req.getParameter("next") != null) {
+			anotherTestUrl += "&next=" + escapeUrl(tv.req.getParameter("next"));
+		}
+		ctx.put("another_test_url", anotherTestUrl);
 	}
 }
